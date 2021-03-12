9906a36d74dcba82b79b4fe58b84fcf9b1ba6038
cchenmmichaeli <cchenmmichaeli@gmail.com>
2021-03-12 23:34:37 +0200

    comment

--- src/screens/SightSeeingAndNightLife/ShowSightSeeingAndNightLife.js
+++ src/screens/SightSeeingAndNightLife/ShowSightSeeingAndNightLife.js
@@ -228,7 +228,7 @@ class ShowSightSeeing extends Component {
                 
             
             })
-//12dsdsdsds
+//12dsdsdsdsdsds
             .then(something => { 
                 
                 for (let i = 0; i < savings.try2.length; i++) {
